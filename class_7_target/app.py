abb36807e4c8f8841f2e2bb037bcecfe72a43379
Qiang Chen <chenqiang19860101@gmail.com>
2026-04-18 18:59:23 -0700

    x

--- class_7_target/app.py
+++ class_7_target/app.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 def index_entrance():
     return render_template('index.html'), 200
 
+@app.route('/my_backend', methods=['POST'])
+def my_backend():
+    my_input = json.loads(request.data)
+    
+    return json.dumps({
+        "sum": my_input["num1"] + my_input["num2"]
+    })
 
 app.run(
     host="127.0.0.1",
--- class_7_target/static/index.js
+++ class_7_target/static/index.js
@@ -1,18 +1,57 @@
 
+CLICK_BUTTON = document.getElementById('clickButton')
+CLICK_BUTTON.addEventListener('click', clickButtonHandler);
+
+CONTENT_DIV = document.getElementById('my_content')
+
+CLICK_TIMES = 0
+
 function clickButtonHandler() {
-    alert('Button was clicked!');
-}
+    console.log("Button was clicked.")
+    CLICK_TIMES +=1
 
-document.getElementById('clickButton').addEventListener('click', clickButtonHandler);
+    new_elmt = document.createElement("div")
+    new_elmt.id=`click_counting_${CLICK_TIMES}`
+    new_elmt.innerHTML=`You clicked the Button ${CLICK_TIMES} times`
 
+    CONTENT_DIV.appendChild(new_elmt)
 
-function fileInputHandler(event) {
-    const file = event.target.files[0];
-    if (file) {
-        alert(`Selected file: ${file.path} ${file.name}`);
-    } else {
-        alert('No file selected.');
+    if (CLICK_TIMES > 3) {
+        to_remove_id = `click_counting_${CLICK_TIMES - 3}`
+        to_remove_elmt = document.getElementById(to_remove_id)
+        to_remove_elmt.remove()
     }
 }
 
-document.getElementById('fileInput').addEventListener('change', fileInputHandler);
+
+BACKEND_BUTTON = document.getElementById('backendCall')
+BACKEND_BUTTON.addEventListener('click', backendButtonHandler);
+
+async function backendButtonHandler() {
+    try{
+        NUM1_TEXTBOX = document.getElementById('num1_input')
+        NUM2_TEXTBOX = document.getElementById('num2_input')
+
+        response = await fetch("/my_backend", {
+            method: "POST",
+            body: JSON.stringify({
+                "num1": parseInt(NUM1_TEXTBOX.value),
+                "num2": parseInt(NUM2_TEXTBOX.value)
+            })
+        })
+        
+        response_text = await response.text()
+
+        response_obj = JSON.parse(response_text)
+
+        new_elmt = document.createElement("div")
+        new_elmt.innerHTML = response_obj["sum"]
+
+        CONTENT_DIV.appendChild(new_elmt)
+    }
+    catch(e) {
+        alert(e)
+    }
+}
+
+
